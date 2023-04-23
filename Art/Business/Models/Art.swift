2a1d2289f42a33cbe4d81febce20b47eb4b1fe93
Sander de Koning <sander@into.ventures>
2023-04-24 00:49:45 +0200

    docs: add art page hashable note

--- Art/Business/Models/Art.swift
+++ Art/Business/Models/Art.swift
@@ -10,6 +10,8 @@ import Foundation
 // Defining as an alias of the response object; in reality this will likely have its own schema
 typealias Art = CollectionResponse.ArtObject
 
+// FIXME: there is a theoretical chance of identical page + art hashables from the API
+// The API can return identical art objects when sorted by maker
 struct ArtPage: Hashable {
     let art: Art
     let page: Int
